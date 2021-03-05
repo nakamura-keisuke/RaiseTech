66f3093d6c14dfc587be6c5cb3fb1d9dba61cd1d
中村佳輔 <>
2021-03-06 07:30:29 +0900

    Raise Tech 第３回目　課題

---
+++ Kadai3/src/main/java/com/example/demo/Kadai3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kadai3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kadai3Application.class, args);
+	}
+
+}
