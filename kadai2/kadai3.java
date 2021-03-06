871cc2ec95752f9ada464180c0bc897f3701eff8
中村佳輔 <>
2021-03-06 18:51:12 +0900

    ２回目課題のkadai3を編集

--- kadai2/kadai3.java
+++ kadai2/kadai3.java
@@ -1,9 +1,15 @@
 package second;
 
+import java.time.LocalDateTime;
+
 public class kadai3 {
-	
+
 	public static void main(String[]args) {
-		//3年後の今日が何曜日か表示
+		//３年後の今日の曜日
+
+	    LocalDateTime day1 = LocalDateTime.now();
+	    LocalDateTime day2 = day1.plusYears(3);
+	    System.out.println(day2.getDayOfWeek());
 	}
 
 }
