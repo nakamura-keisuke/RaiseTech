d9a727053293e43200e3aea19e21052a5488b57b
中村佳輔 <>
2021-03-06 18:58:11 +0900

    adai5の編集

--- kadai2/kadai5.java
+++ kadai2/kadai5.java
@@ -1,10 +1,25 @@
 package second;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class kadai5 {
 
 	public static void main(String[] args) {
-		// コレクション型は割愛。String型のListかMapを作成して、
-		//その内容をコンソール画面に表示してみよう。Listに入れる内容は何でもOK。ただし複数入れる事。
-	}
+	//ListとMapの作成
+
+    ArrayList <String> color = new ArrayList<String>();
+    color.add("red");
+    color.add("blue");
+    color.add("yellow");
+    System.out.println(color.get(0));
+
+    HashMap<Integer, String> season = new HashMap();
+    season.put(1,"spring");
+    season.put(2,"summer");
+    season.put(3,"fall");
+    season.put(4,"winter");
+    System.out.println(season.get(0));
+    }
 
 }
